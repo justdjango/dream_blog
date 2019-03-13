2dd2d00523c2386f24e56b6f145481ad5959abd5
mattfreire <freire1996@gmail.com>
2019-03-13 22:06:33 +0200

    Debug false

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k*b$$!%hiswnhp2pad)6(u4x_g)r5w%@&zlv&@2hrz4#f3+#57'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
