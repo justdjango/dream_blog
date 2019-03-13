9eb0009f46f645740c4bec948ad46e8ba895ff74
mattfreire <freire1996@gmail.com>
2019-03-13 21:54:43 +0200

    allowed host

--- blog/settings.py
+++ blog/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'k*b$$!%hiswnhp2pad)6(u4x_g)r5w%@&zlv&@2hrz4#f3+#57'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*.justdjangoblog.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
