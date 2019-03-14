4cf64e27b3d86c136f4227afc57d85bd0a587e84
mattfreire <freire1996@gmail.com>
2019-03-14 21:51:26 +0200

    whitenoise

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -1,8 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
-# from whitenoise.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
 application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
