46e813e635207ef41fd3edf29955a437a5f8c7fb
mattfreire <freire1996@gmail.com>
2019-03-14 21:42:55 +0200

    remove whitenoise

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
