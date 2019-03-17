37c704f0d8f1aec5ab8c45ba3608b4cdd1371503
mattfreire <freire1996@gmail.com>
2019-03-17 18:41:19 +0200

    Changed static path

--- blog/settings.py
+++ blog/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Tinymce
 
