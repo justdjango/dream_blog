65c40db9b2207cefbcc117b226b2172c5b66c089
oluwakayode-a <khaox07@gmail.com>
2019-12-03 20:11:35 +0100

    fixed tinymce import error on forms.py and models.py

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from tinymce import TinyMCE
+from tinymce.widgets import TinyMCE
 from .models import Post, Comment
 
 
@@ -17,8 +17,8 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'overview', 'content', 'thumbnail', 
-        'categories', 'featured', 'previous_post', 'next_post')
+        fields = ('title', 'overview', 'content', 'thumbnail',
+                  'categories', 'featured', 'previous_post', 'next_post')
 
 
 class CommentForm(forms.ModelForm):
@@ -28,6 +28,7 @@ class CommentForm(forms.ModelForm):
         'id': 'usercomment',
         'rows': '4'
     }))
+
     class Meta:
         model = Comment
         fields = ('content', )
--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,4 @@
-from tinymce import HTMLField
+from tinymce.models import HTMLField
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -51,10 +51,6 @@ class Post(models.Model):
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField()
-    previous_post = models.ForeignKey(
-        'self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
-    next_post = models.ForeignKey(
-        'self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.title
